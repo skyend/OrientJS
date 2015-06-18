77c2c5f400b562c040ee1cbc03df05222ebd5c48
Yoon seonwoong <swyoon@i-on.net>
2015-06-18 15:50:29 +0900

    Accordian Add

--- js/builder.js
+++ js/builder.js
@@ -4,8 +4,11 @@
 
 $(function() {
     console.log( "ready!" );
-    $("#menu-list li").on("click", "a", function(){
-        $(this).attr("class", "actvie");
-        console.log( ".on" );
+    $("#menu-list li a").on("click", function(e){
+        $("#menu-list i").find("span").remove();
+        $("#menu-list i").removeClass("active");
+        $(e.target).addClass("active");
+        $(e.target).append("<span class='glyphicon glyphicon-triangle-left tri'></span>");
+
     });
 });
