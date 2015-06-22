4bfd39ee1ccaf5c8ff0a190fa566a4df98937b7a
Yoon seonwoong <swyoon@i-on.net>
2015-06-22 16:05:18 +0900

    add html5 drag&drop sortable library

--- js/builder.js
+++ js/builder.js
@@ -2,9 +2,58 @@
  * Created by seonwoong on 15. 6. 16..
  */
 
-var gridAccordian = React.createClass({
+var BuildMode = React.createClass({
     render: function () {
-        return
+        return (
+            <div className="accordion">
+                <dl>
+                    <dt className="title"><a href="#accordion1" aria-expanded="false"
+                                             aria-controls="accordion1">LAYOUT</a></dt>
+                    <dd className="item" id="accordion1" aria-hidden="true">
+                        <div className="grid">Grid 1</div>
+                        <div className="grid">Grid 2</div>
+                    </dd>
+                    <dt className="title"><a href="#accordion2" aria-expanded="false" aria-controls="accordion2">PLAIN</a>
+                    </dt>
+                    <dd className="item" id="accordion2" aria-hidden="true">
+                        <div>Title</div>
+                        <div>Address</div>
+                        <div>Form</div>
+                    </dd>
+                </dl>
+            </div>);
+        //<div className="accordion">
+        //    <dl>
+        //        <dt><a href="#accordion1" aria-expanded="false" aria-controls="accordion1"
+        //               className="accordionTitle js-accordionTrigger">LAYOUT</a></dt>
+        //        <dd className="accordionItem is-collapsed" id="accordion1" aria-hidden="true">
+        //            <p className="grid">Grid 1</p>
+        //            <p className="grid">Grid 2</p>
+        //        </dd>
+        //        <dt><a href="#accordion2" aria-expanded="false" aria-controls="accordion2"
+        //               className="accordionTitle js-accordionTrigger">PLAIN</a></dt>
+        //        <dd className="accordionItem is-collapsed" id="accordion2" aria-hidden="true">
+        //            <p>Title</p>
+        //
+        //            <p>Address</p>
+        //
+        //            <p>Form</p>
+        //        </dd>
+        //    </dl>
+        //</div>);
+    }
+});
+
+var DeviceViewMode = React.createClass({
+    render: function () {
+        return (
+            <div id="device-mode" className="device-mode">
+                <dt>DEVICE MODE</dt>
+                <dd>Desktop <i className="fa fa-desktop"></i></dd>
+                <dd>Tablet <i className="fa fa-tablet"></i></dd>
+                <dd>Mobile <i className="fa fa-mobile"></i></dd>
+            </div>
+        )
     }
 });
 
@@ -17,13 +66,14 @@ $(function () {
         $(e.target).append("<span class='glyphicon glyphicon-triangle-left tri'></span>");
     });
     $("#menu-1").on("click", function () {
-        $("#side-contents").empty();
-        $("#side-contents").append('<div class="accordion"> <dl> <dt> <a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordionTitle js-accordionTrigger">LAYOUT</a> </dt> <dd class="accordionItem is-collapsed" id="accordion1" aria-hidden="true"> <p id="grid">Grid 1</p> <p>Grid 2</p> </dd> <dt> <a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordionTitle js-accordionTrigger">PLAIN</a> </dt> <dd class="accordionItem is-collapsed" id="accordion2" aria-hidden="true"> <p>Title</p> <p>Address</p> <p>Form</p> </dd> </dl> </div>');
-        accFuncLoad();
+        React.render(
+            <BuildMode />, document.getElementById('side-contents')
+        );
     });
     $("#menu-2").on("click", function () {
-        $("#side-contents").empty();
-        $("#side-contents").append('<div id="device-mode" class="device-mode"> <dt>DEVICE MODE</dt> <dd>Desktop <i class="fa fa-desktop"></i></dd> <dd>Tablet <i class="fa fa-tablet"></i></dd> <dd>Mobile <i class="fa fa-mobile"></i></dd> </div>');
+        React.render(
+            <DeviceViewMode />, document.getElementById('side-contents')
+        );
     });
 
 });
