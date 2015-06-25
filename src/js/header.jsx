0ed41d965c4e458a3106b1a3649b99376cd67b80
Yoon seonwoong <swyoon@i-on.net>
2015-06-25 17:07:08 +0900

    put head

---
+++ src/js/header.jsx
@@ -0,0 +1,16 @@
+module.exports = function(){
+    var React = require('react');
+    return React.createClass({
+        render: function () {
+            return (
+                <ul className="nav navbar-right">
+                    <li>
+                        <a>
+                            <i className="fa fa-columns"></i>
+                        </a>
+                    </li>
+                </ul>
+            )
+        }
+    });
+};
