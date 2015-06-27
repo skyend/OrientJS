c21730acf081a5c13b288b7bc65b06ab0fee2778
jinwoong Han imac <theskyend0@gmail.com>
2015-06-28 04:32:43 +0900

    구조 설정 및 EventEmitter 추가, extends 메소드를 추가하여 객체 확장(계승)기능 추가

---
+++ src/js/package/builder.Utils.js
@@ -0,0 +1,24 @@
+/**
+ * Builder,Utils
+ * 빌더 Uitls
+ *
+ * 
+ *
+ * 
+ */
+
+(function(){
+		exports.extends = function( _source, _target, _arguments ){
+				var F = function(){
+						return _source.apply(this, _arguments);
+				}
+				
+				F.prototype = _source.prototype;
+			
+				var inst = new F();
+
+				for( attr in inst ){	
+						_target[attr] = inst[attr];
+				}
+		}
+})();
