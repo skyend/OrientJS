8492570ad07537e77ed1bb8c87b8c212d844d90a
Yoon seonwoong <swyoon@i-on.net>
2015-07-27 17:14:49 +0900

    자잘한 정리

--- src/js/ui/UIService.jsx
+++ src/js/ui/UIService.jsx
@@ -6,7 +6,6 @@
  * Requires(css) : Screen.less
  */
 var $ = require('jquery');
-
 require('jquery-ui');
 (function () {
     require('./UIService.less');
@@ -293,7 +292,6 @@ require('jquery-ui');
         },
 
         render() {
-            var self = this;
             var leftMenuList = LeftMenuListConfig;
             var rightMenuList = RightMenuListConfig;
             var leftNaviRefKey = "LeftNavigation";
@@ -334,9 +332,6 @@ require('jquery-ui');
     });
 
     module.exports = UIService;
-
-
-
 
     function loadPanel(pageName, callback) {
         try {
