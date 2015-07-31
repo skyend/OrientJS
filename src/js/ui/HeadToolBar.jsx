3dd01b187a06718349c2171f4b9eea673cbe6d17
Yoon seonwoong <swyoon@i-on.net>
2015-07-31 18:08:46 +0900

    #11
    1. HeadToolBar - Add login UI

--- src/js/ui/HeadToolBar.jsx
+++ src/js/ui/HeadToolBar.jsx
@@ -10,8 +10,21 @@
 (function () {
     require('./HeadToolBar.less');
     var React = require("react");
-
+    var $ = require("jquery");
     var HeadToolBar = React.createClass({
+        componentDidMount(){
+            var status = true;
+            $(".login-form").hide();
+            $('.user-info').on("click", function () {
+                if (status) {
+                    $(".login-form").slideUp().slideToggle("fast");
+                    status = false;
+                } else {
+                    $(".login-form").slideDown().slideToggle("fast");
+                    status = true;
+                }
+            });
+        },
         render: function () {
             return (
                 <header id="ui-header">
@@ -23,15 +36,22 @@
                         </li>
                         <li>
                             <a className="user-info">
-                                <i className="fa fa-user"> i-on</i>
+                                <i className="fa fa-sign-in"> Login</i>
                             </a>
+
+                            <form action="#" className="login-form" method="post">
+                                <input type="text" placeholder="ID" className="login-input"/>
+                                <input type="password" placeholder="PASSWORD" className="login-input"/>
+                                <input type="submit" className="login-button" value="Login" tabindex="3"/>
+
+                                <div className="remember">Remember?<input type="checkbox" className="checkbox"/></div>
+                            </form>
                         </li>
                     </ul>
                 </header>
             )
         }
     });
-
     module.exports = HeadToolBar;
 
 })();
