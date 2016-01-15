611bfcf396894cc6a8ef56ff065cbccf4115e987
한진웅 <theskyend0@i-on.net>
2016-01-15 10:53:32 +0900

    Ajax 콜백인자에 Response 스테이터스 코드 포함

--- src/js/serviceCrew/StandAloneLib/Loader.js
+++ src/js/serviceCrew/StandAloneLib/Loader.js
@@ -4,12 +4,12 @@ class ServiceResourceLoader {
   static loadConfig(_complete) {
     request.get('./config/config.json')
       .end(function(_err, _result) {
-        //  console.log(_err, _result);
+        console.log(_err, _result);
         if (_err !== null) {
           console.warn(_err);
           _complete(null);
         } else {
-          _complete(_result.body);
+          _complete(_result.body, _result.statusCode);
         }
       });
   }
@@ -17,12 +17,12 @@ class ServiceResourceLoader {
   static loadFragment(_name, _complete) {
     request.get('./fragments/' + _name + '.html')
       .end(function(_err, _result) {
-        //  console.log(_err, _result);
+        console.log(_err, _result);
         if (_err !== null) {
           console.warn(_err);
           _complete(null);
         } else {
-          _complete(_result.text);
+          _complete(_result.text, _result.statusCode);
         }
       });
   }
@@ -30,21 +30,21 @@ class ServiceResourceLoader {
   static loadSharedElementNode(_name, _complete) {
     request.get('./fragments/shared/' + _name + '.html')
       .end(function(_err, _result) {
-        // console.log(_err, _result);
+        console.log(_err, _result);
         //if (_err !== null) throw new Error('Fail load a shared [' + _name + ']');
-        _complete(_result.text);
+        _complete(_result.text, _result.statusCode);
       });
   }
 
   static loadAPISource(_name, _complete) {
     request.get('./apisources/' + _name + '.json')
       .end(function(_err, _result) {
-        // console.log(_err, _result);
+        console.log(_err, _result);
         if (_err !== null) {
           console.warn(_err);
           _complete(null);
         } else {
-          _complete(_result.body);
+          _complete(_result.body, _result.statusCode);
         }
 
 
