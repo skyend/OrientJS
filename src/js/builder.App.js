c14d73ce8e2a9aa82a9cae9542269b29bbcdcf90
한진웅 <theskyend0@i-on.net>
2016-01-19 15:22:51 +0900

    Master 브랜치 빌더서버  ip 변경

--- src/js/builder.App.js
+++ src/js/builder.App.js
@@ -20,7 +20,7 @@ import Viewer from './serviceCrew/Viewer.js';
 import PreviewScene from './ui.workspace/Context/PageContext/PreviewScene.jsx';
 import React from 'react';
 
-window.gelateriaHost = 'localhost:8090';
+window.gelateriaHost = '125.131.88.146:8090';
 
 var App = function() {
   window.app = this;
