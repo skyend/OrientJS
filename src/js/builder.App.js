e74f1e745ef97f93ee272320a92542546317cf4d
Jinwoong Han <theskyend0@i-on.net>
2016-02-02 11:56:51 +0900

    서버 포함용 gelatoHost 생략

--- src/js/builder.App.js
+++ src/js/builder.App.js
@@ -20,7 +20,7 @@ import Viewer from './serviceCrew/Viewer.js';
 import PreviewScene from './ui.workspace/Context/PageContext/PreviewScene.jsx';
 import React from 'react';
 
-window.gelateriaHost = 'localhost:9080';
+window.gelateriaHost = '';
 const FIXED_ICE_HOST = 'http://125.131.88.75:8080';
 
 var App = function() {
